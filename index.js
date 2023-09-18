01b252b388290362f7c4e9865db24b79a4cf9a3f
deepak-thak <deepakthakur44556@gmail.com>
2023-09-18 13:59:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+new Typed('#typed',{
+    strings : ['Web devloper', 'Designer'],
+    typeSpeed : 100,
+    delaySpeed : 1100,
+    loop : true
+  });
+
+  new Typed('#typed2',{
+    strings : ['Thakur'],
+    typeSpeed : 75,
+    delaySpeed : 110,
+    loop : true
+  });
